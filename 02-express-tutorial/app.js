11bc2cbc57bc53d28f532665b07a752bfc33fb74
Dev-Emmyy <emmanuelugochukwu2000@gmail.com>
2024-05-12 18:04:50 +0100

    commit

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,9 +1,23 @@
 const http = require("http");
 
 const server = http.createServer((req,res) => {
-    res.writeHead(200,{"content-type" : "type/html"});
-    res.write("<h1>Hello World</h1>")
-    res.end()
+    const url = req.url;
+
+    if (url === "/") {
+        res.writeHead(200,{"Content-Type" : "text/html"});
+        res.write("<h1>Home Page</h1>");
+        res.end();
+    }
+    else if (url === "/about") {
+        res.writeHead(200,{"Content-Type" : "text/html"});
+        res.write("<h1>About page</h1>");
+        res.end();
+    } 
+    else {
+        res.writeHead(404,{"Content-Type" : "text/html"});
+        res.write("<h1>Page not found</h1>");
+        res.end();
+    }
 });
 
-server.listen(5000)
+server.listen(5000);
