148872f85e9ae0c5eb3055a576e8da88ecdf866e
Emmanuel Ugochukwu <emmanuelugochukwu2000@gmail.com>
2024-05-29 00:24:17 +0100

    commit

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,41 +1,17 @@
-const http = require("http");
-const {readFileSync} = require("fs");
+const express = require("express");
+const path = require("path");
+const app = express();
 
-const Homepage = readFileSync("./navbar-app/index.html");
-const Homepage_Css = readFileSync("./navbar-app/styles.css");
-const Homepage_Logo = readFileSync("./navbar-app/logo.svg");
-const Homepage_Logic = readFileSync("./navbar-app/browser-app.js");
+app.use(express.static("./public"));
 
-
-const server = http.createServer((req,res) => {
-    const url = req.url;
-    console.log(url);
-
-    if (url === "/") {
-        res.writeHead(200,{"Content-Type" : "text/html"});
-        res.write(Homepage);
-        res.end();
-    }
-    else if (url === "/styles.css") {
-        res.writeHead(200,{"Content-Type" : "text/css"});
-        res.write(Homepage_Css);
-        res.end();
-    } 
-    else if (url === "/browser-app.js") {
-        res.writeHead(200,{"Content-Type" : "text/javascript"});
-        res.write(Homepage_Logic);
-        res.end();
-    } 
-    else if (url === "/logo.svg") {
-        res.writeHead(200,{"Content-Type" : "image/svg+xml"});
-        res.write(Homepage_Logo);
-        res.end();
-    } 
-    else {
-        res.writeHead(404,{"Content-Type" : "text/html"});
-        res.write("<h1>Page not found</h1>");
-        res.end();
-    }
+app.get("/",(req,res) => {
+    res.sendFile(path.resolve(__dirname,"./navbar-app/index.html"));
 });
 
-server.listen(5000);
+app.use("*",(req,res) => {
+    res.status(404).send("Resource not found");
+})
+
+app.listen(5000, () => {
+    console.log("Server is now running")
+});
